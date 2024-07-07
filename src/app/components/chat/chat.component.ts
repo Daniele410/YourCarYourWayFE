f4f62f56308c07945e4f480eea3232ff21bad8ae
Daniele410 <miraglia.daniele410@gmail.com>
2024-07-07 16:27:00 +0200

    Add message and try sendMessage two users

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../../services/chat.service';
+import { ChatMessage } from '../../models/chat-message';
+import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit{
 
+  messageInput: string = '';
+  userId: string = '';
+  messageList: any[] = [];
+
+  constructor(private chatService: ChatService, private route: ActivatedRoute) { 
+
+  }
+
+  ngOnInit(): void {
+    this.chatService.joinRoom("ABC");
+    this.userId= this.route.snapshot.params['userId'];
+  }
+
+  sendMessage(){
+    const chatMessage = {
+      message: this.messageInput,
+      user: this.userId
+    }as ChatMessage;
+
+    this.chatService.sendMessage("ABC", chatMessage);
+    this.messageInput = '';
+  }
 }

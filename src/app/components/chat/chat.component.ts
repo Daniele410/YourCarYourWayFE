08e6ae3dfc4ccb9e4f559f78aa75db9dccc6d542
Daniele410 <miraglia.daniele410@gmail.com>
2024-07-15 14:04:21 +0200

    add getUserRole

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -5,11 +5,16 @@ import { ActivatedRoute } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgClass, NgFor } from '@angular/common';
 import { isNgTemplate } from '@angular/compiler';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [FormsModule, NgFor, NgClass],
+  imports: [FormsModule, NgFor, NgClass, MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
@@ -18,6 +23,7 @@ export class ChatComponent implements OnInit {
   messageInput: string = '';
   userId: string = '';
   messageList: any[] = [];
+
 
   constructor(private chatService: ChatService, private route: ActivatedRoute) {
 
@@ -28,6 +34,16 @@ export class ChatComponent implements OnInit {
     this.userId = this.route.snapshot.params['userId'];
     this.lisenerMessage();
   }
+  getUserRole(): string {
+    if (this.userId === 'user') {
+      return 'user';
+    } else if (this.userId === 'admin') {
+      return 'admin';
+    } else {
+      return this.userId;
+    }
+  }
+
 
   sendMessage() {
     const chatMessage = {
@@ -41,9 +57,9 @@ export class ChatComponent implements OnInit {
 
   lisenerMessage() {
     this.chatService.getMessageSubject().subscribe((messages: any) => {
-      this.messageList = messages.map((item:any) => ({
+      this.messageList = messages.map((item: any) => ({
         ...item,
-        message_side: item.user === this.userId ? 'sender' : 'receiver' 
+        message_side: item.user === this.userId ? 'sender' : 'receiver'
 
       }));
     });
